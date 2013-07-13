5355918addd9e805f292fa2c68384dd93b5c9558
Etienne Caron <etienne.caron@gmail.com>
2013-07-13 00:41:48 -0400

    Adding new menu items under res/menu/main_activity.xml, you can see how these are activated in MyActivity.java Note all the varying icons added under the various drawable screen densities.

--- src/com/gdg/montreal/android/UpAndRunningHamburgerAction/MyActivity.java
+++ src/com/gdg/montreal/android/UpAndRunningHamburgerAction/MyActivity.java
@@ -2,6 +2,9 @@ package com.gdg.montreal.android.UpAndRunningHamburgerAction;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MyActivity extends Activity {
 	/**
@@ -11,5 +14,30 @@ public class MyActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+	}
+
+	/**
+	 * Needed to build the action bar menu items.
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.main_activity,menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+			case R.id.bugdroid :
+				// TODO: Do something
+				return true ;
+			case R.id.box :
+				// TODO: Do something
+				return true ;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 }
